2d5d819d30dfd4ec6c28f1cd3dd13c750add24a8
Ivan Orpineda <ivan.orpineda.dev@gmail.com>
2026-07-05 23:01:16 -0500

    fix page import

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { CotizadorApp } from "@/components/CotizadorApp";
-import { ProductionMenuController } from "@/components/ProductionMenuController";
 
 export default function Home() {
-  return (
-    <>
-      <ProductionMenuController />
-      <CotizadorApp />
-    </>
-  );
+  return <CotizadorApp />;
 }
